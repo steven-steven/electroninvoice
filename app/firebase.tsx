3b9ab94b9a2f374c77b9440c694ec32e5eec5ee5
Steven Steven <steven.steven@shopify.com>
2021-08-01 20:33:40 -0400

    Make upgrades & dependency checks

--- app/firebase.tsx
+++ app/firebase.tsx
@@ -1,6 +1,5 @@
-import { firebase } from '@firebase/app';
-import '@firebase/auth';
-import '@firebase/database';
+import firebase from 'firebase/app';
+import 'firebase/database';
 import config from './config.json';
 
 const firebaseConfig = {
